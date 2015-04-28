0320ea4a20f73c23f9dfd42b535db34d69fd6134
Matt Jackson <email@mwjackson.net>
2015-04-28 07:17:34 +0100

    reversed 31155783bbd401e177915c6e3bd65c02501d32a3
    users should verify email address first, before going any further.
    this will prevent unneccessary activation of users until we are sure they
    own the email address

--- allauth/account/utils.py
+++ allauth/account/utils.py
@@ -105,13 +105,6 @@ def perform_login(request, user, email_verification,
     email is essential (during signup), or if it can be skipped (e.g. in
     case email verification is optional and we are only logging in).
     """
-    # Local users are stopped due to form validation checking
-    # is_active, yet, adapter methods could toy with is_active in a
-    # `user_signed_up` signal. Furthermore, social users should be
-    # stopped anyway.
-    if not user.is_active:
-        return HttpResponseRedirect(reverse('account_inactive'))
-
     from .models import EmailAddress
     has_verified_email = EmailAddress.objects.filter(user=user,
                                                      verified=True).exists()
@@ -126,6 +119,14 @@ def perform_login(request, user, email_verification,
             send_email_confirmation(request, user, signup=signup)
             return HttpResponseRedirect(
                 reverse('account_email_verification_sent'))
+
+    # Local users are stopped due to form validation checking
+    # is_active, yet, adapter methods could toy with is_active in a
+    # `user_signed_up` signal. Furthermore, social users should be
+    # stopped anyway.
+    if not user.is_active:
+        return HttpResponseRedirect(reverse('account_inactive'))
+
     try:
         get_adapter().login(request, user)
         response = HttpResponseRedirect(
