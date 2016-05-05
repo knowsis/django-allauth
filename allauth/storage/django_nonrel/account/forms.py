e140663b199fd3125ecc021dfeab7b74fd64833a
Matt Jackson <email@mwjackson.net>
2016-05-05 16:41:12 +0100

    fixed model ref in resetform

--- allauth/storage/django_nonrel/account/forms.py
+++ allauth/storage/django_nonrel/account/forms.py
@@ -1,4 +1,8 @@
+from allauth.account.adapter import get_adapter
 from allauth.account.forms import ResetPasswordForm
+from allauth.utils import get_user_model
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 
 class ResetPasswordForm(ResetPasswordForm):
@@ -6,7 +10,7 @@ class ResetPasswordForm(ResetPasswordForm):
     def clean_email(self):
         email = self.cleaned_data["email"]
         email = get_adapter().clean_email(email)
-        self.users = User.objects.filter(email__iexact=email)
+        self.users = get_user_model().objects.filter(email__iexact=email)
 
         if not self.users.exists():
             raise forms.ValidationError(_("The e-mail address is not assigned"
